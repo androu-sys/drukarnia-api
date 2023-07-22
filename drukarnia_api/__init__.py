cb8ddac69a10b73a058e8ef3b966e8827a6da48e
Andrii <modicum.geotag.0v@icloud.com>
2023-07-22 19:17:51 +0200

    few updates

--- drukarnia_api/__init__.py
+++ drukarnia_api/__init__.py
@@ -1,6 +1,5 @@
 # This line of code will allow shorter imports
 from drukarnia_api.objects import *
 from drukarnia_api.network import *
-from drukarnia_api.shortcuts import *
 
 print("Слава Україні 🇺🇦")
