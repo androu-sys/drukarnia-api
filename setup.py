8fecdd9d71d76346141285be60910413218b9cb9
Andrii <modicum.geotag.0v@icloud.com>
2023-07-24 14:08:26 +0200

    setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(HERE, 'requirements.txt')) as f:
 # This call to setup() does all the work
 setup(
     name="drukarnia-api",
-    version="1.1.0",
+    version="1.1.1",
     description="wrapper for the Drukarnia API",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -29,6 +29,10 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent"
