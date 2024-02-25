40971d85e4fda4ffd1ca5f8258b9b17bb0d7ae39
androu <modicum.geotag.0v@icloud.com>
2024-02-25 17:12:42 +0100

    fixed MyPy error

--- drukarnia_api/dto/utils.py
+++ drukarnia_api/dto/utils.py
@@ -1,9 +1,9 @@
 import attrs
-from typing import Callable
+from typing import Any, Callable
 from drukarnia_api.dto.base import BaseDTO
 
 
-def starts_with(prefix: str) -> Callable[[attrs.Attribute, str], None]:
+def starts_with(prefix: str) -> Callable[[Any, attrs.Attribute, str], None]:
     """
     Creates a custom validator function that checks if a string starts with the given prefix.
 
@@ -14,11 +14,12 @@ def starts_with(prefix: str) -> Callable[[attrs.Attribute, str], None]:
         Callable[[object, attr.Attribute, str], None]: A validator function to be used with attrs.
     """
 
-    def validate(instance, attribute: attrs.Attribute, value: str) -> None:
+    def validate(instance: Any, attribute: attrs.Attribute, value: str) -> None:
         """
         Validates whether the given value starts with the specified prefix.
 
         Args:
+            instance (any): The instance that method is called on.
             attribute (attr.Attribute): The attribute being validated.
             value (str): The value of the attribute to validate.
 
