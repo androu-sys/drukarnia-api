869ba2a7c45bc952bbfee5f6d5c3bd29aa1e117f
androu <modicum.geotag.0v@icloud.com>
2024-02-25 16:05:29 +0100

    filled the SocialsModel

--- drukarnia_api/models/socials.py
+++ drukarnia_api/models/socials.py
@@ -6,4 +6,11 @@ from attrs import frozen
 @frozen
 class SocialsModel(BaseModel):
     telegram: Optional[str]
+    instagram: Optional[str]
+    twitter: Optional[str]
+    bluesky: Optional[str]
+    facebook: Optional[str]
+    linkedin: Optional[str]
+    youtube: Optional[str]
+    tiktok: Optional[str]
     website: Optional[str]
